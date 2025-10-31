c3d1469c27ac9c403c6c40e2d205e554d8218073
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-10-31 10:38:19 +0000

    Task 2: Add cached property list view

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15) 
+def property_list(request):
+    """
+    A view that retrieves all properties from the database,
+    serializes them into a list of dictionaries, and returns
+    them as a JSON response. The response is cached for 15 minutes.
+    """
+    
+    properties = Property.objects.all()
+    
+    data = list(properties.values()) 
+    
+    return JsonResponse(data, safe=False)
